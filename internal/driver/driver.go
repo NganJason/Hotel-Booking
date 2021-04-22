446b5e0b874881848313f839f07bea77927abe5b
Jason Ngan <jason.ngan@shopee.com>
2021-04-22 08:59:54 +0800

    adding db connection

---
+++ internal/driver/driver.go
@@ -0,0 +1,46 @@
+package driver
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+type DB struct {
+	SQL *sql.DB
+}
+
+var dbConn = &DB{}
+
+const maxOpenDBConn = 10
+const maxIdleDBConn = 5
+const maxDbLifeTime = 5 * time.Minute
+
+func ConnectSQL(dsn string) (*DB, error) {
+	d, err := NewDatabase(dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	d.SetMaxOpenConns(maxOpenDBConn)
+	d.SetMaxIdleConns(maxIdleDBConn)
+	d.SetConnMaxLifetime(maxDbLifeTime)
+
+	dbConn.SQL = d
+	return dbConn, nil
+}
+
+func NewDatabase(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
